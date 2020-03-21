3cfb214eaf50d4291bb3ee56ea0721126974b293
Peter Rose <peterroseq1@gmail.com>
2020-03-21 20:11:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,42 @@
-class Song 
-  attr_accessor :name :artist :genres 
-  @@count = 0 
-  
-  def initialize(name, genre) 
-    @@count+=1 
-    @@genres = [""]
-    @@artists = [""]
+class Song
+
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(song_name,artist,genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
-  def count 
+
+  def self.count 
     @@count
   end
-  def genres
-    @@genres.uniq  
-  end
-  def artists
+
+  def self.artists
     @@artists.uniq
   end
-  def genre_count
-    {@@genres.size => @@count 
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
+end
+
   
